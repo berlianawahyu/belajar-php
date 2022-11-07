7147c125af2005b4e5e3f402b5cf5f556a6e80f8
berliana wahyu pratiwi <berlianawp05@gmail.com>
2022-11-07 10:32:22 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+$nama = "Berliana";
+$umur = 17;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Iyul";
+$umurAdik = 15;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia 
+$umurAdik tahun.<br>";
+
+
+$selisihumur = $umur - $umurAdik;
+echo "selisih usia saya dan adik saya adalah $selisihumur tahun."
+
+?>
